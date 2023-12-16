28fd8f5058e9287868c6b4aaadd0c989f8890f29
Syed Rafay <rafaysyed637@gmail.com>
2023-12-16 16:51:57 +0500

    render cards

--- app.js
+++ app.js
@@ -1,191 +1,116 @@
-[
+let Myarr = [
     {
         id: 4,
         title: "Dummy Data1",
-        price: 382,
+        price: "Rs/-$382",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-        product_oder: "Oder1"
+        product_oder: "Buy Now"
     },
-]
-const obj = document.getElementById("1a")
-const arr = {
-    title: "Dummy Data1",
-    price: 382,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
-    Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-    product_oder: "Oder1",
-}
-for (let i = 0; i < arr.length; i++) {
-    const cardHTML =
-        `<div class="card" style="width: 22rem;">
-            <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
-            <div class="card-body">
-                <h5 class="card-title">${arr[i].title}</h5>
-                <p class="card-text">${arr[i].description}</p>
-                ${arr[i].prise}
-                <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
-            </div>
-        </div>`
-};
-console.log(arr);
-
-[
     {
         id: 8,
         title: "Dummy Data2",
-        price: 496,
+        price: "Rs/-$496",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-        product_oder: "Oder2"
+        product_oder: "Buy Now"
     },
-]
-const obj1 = document.getElementById("2b")
-const arr1 = {
-    title: "Dummy Data2",
-    price: 496,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
-    Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-    product_oder: "Oder2",
-}
-for (let i = 0; i < arr.length; i++) {
-    const cardHTML =
-        `<div class="card" style="width: 22rem;">
-            <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
-            <div class="card-body">
-                <h5 class="card-title">${arr[i].title}</h5>
-                <p class="card-text">${arr[i].description}</p>
-                ${arr[i].prise}
-                <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
-            </div>
-        </div>`
-};
-console.log(arr1);
-
-[
     {
         id: 12,
         title: "Dummy Data3",
-        price: 572,
+        price: "Rs/-$572",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-        product_oder: "Oder3"
+        product_oder: "Buy Now"
     },
-]
-const obj2 = document.getElementById("3c")
-const arr2 = {
-    title: "Dummy Data3",
-    price: 572,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
-    Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-    product_oder: "Oder3",
-}
-for (let i = 0; i < arr.length; i++) {
-    const cardHTML =
-        `<div class="card" style="width: 22rem;">
-            <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
-            <div class="card-body">
-                <h5 class="card-title">${arr[i].title}</h5>
-                <p class="card-text">${arr[i].description}</p>
-                ${arr[i].prise}
-                <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
-            </div>
-        </div>`
-};
-console.log(arr2);
-
-[
     {
         id: 16,
         title: "Dummy Data4",
-        price: 797,
+        price: "Rs/-$797",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-        product_oder: "Oder4"
+        product_oder: "Buy Now"
     },
-]
-const obj3 = document.getElementById("4d")
-const arr3 = {
-    title: "Dummy Data4",
-    price: 797,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
-    Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-    product_oder: "Oder4",
-}
-for (let i = 0; i < arr.length; i++) {
-    const cardHTML =
-        `<div class="card" style="width: 22rem;">
-            <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
-            <div class="card-body">
-                <h5 class="card-title">${arr[i].title}</h5>
-                <p class="card-text">${arr[i].description}</p>
-                ${arr[i].prise}
-                <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
-            </div>
-        </div>`
-};
-console.log(arr3);
-
-[
     {
         id: 20,
         title: "Dummy Data5",
-        price: 980,
+        price: "Rs/-$980",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-        product_oder: "Oder5"
+        product_oder: "Buy Now"
     },
-]
-const obj4 = document.getElementById("5e")
-const arr4 = {
-    title: "Dummy Data5",
-    price: 980,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
-    Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-    product_oder: "Oder5",
-}
-for (let i = 0; i < arr.length; i++) {
-    const cardHTML =
-        `<div class="card" style="width: 22rem;">
-            <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
-            <div class="card-body">
-                <h5 class="card-title">${arr[i].title}</h5>
-                <p class="card-text">${arr[i].description}</p>
-                ${arr[i].prise}
-                <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
-            </div>
-        </div>`
-};
-console.log(arr4);
-
-[
     {
         id: 24,
         title: "Dummy Data6",
-        price: 806,
+        price: "Rs/-$806",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-        product_oder: "Oder6"
+        product_oder: "Buy Now"
     },
-]
-const obj5 = document.getElementById("6f")
-const arr5 = {
-    title: "Dummy Data6",
-    price: 806,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
-    Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
-    product_oder: "Oder6",
-}
-for (let i = 0; i < arr.length; i++) {
-    const cardHTML =
-        `<div class="card" style="width: 22rem;">
-            <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
-            <div class="card-body">
-                <h5 class="card-title">${arr[i].title}</h5>
-                <p class="card-text">${arr[i].description}</p>
-                ${arr[i].prise}
-                <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
-            </div>
-        </div>`
-};
-console.log(arr5);
+];
+
+let obj = document.getElementById("1a");
+
+let card_Data = Myarr.map((value, index) => {
+    let { id, title, price, description, Image_url, product_oder } = value;
+
+    return `
+    <div class="1a" style="width: 22rem; margin-left: 28rem; border: 1px solid black;">
+        <img src="${Image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title p-2">${title}</h5>
+            <p class="card-text p-2">${description}</p>
+            <p class="card-prise p-2">${price} ${id}</p>
+            <a href="#" class="btn btn-primary m-2">${product_oder}</a>
+        </div>
+    </div>
+    `;
+});
+
+document.write(card_Data);
+obj.innerHTML = value
+
+
+
+
+
+
+
+
+
+
+// ======================EXTRA WORK START=========================
+
+// [
+//     {
+//         id: 4,
+//         title: "Dummy Data1",
+//         price: 382,
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
+//         Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
+//         product_oder: "Oder1"
+//     },
+// ]
+// const obj = document.getElementById("1a")
+// const arr = {
+//     title: "Dummy Data1",
+//     price: 382,
+//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat repudiandae, enim recusandae dolores odit eaque laborum temporibus modi accusantium fugiat earum? Ipsa omnis sit vitae sapiente nostrum laudantium neque?",
+//     Image_url: "https://cdn05.carsforsale.com/00e7818a91f451732249f04173c1a3058f/480x360/2019-dodge-challenger-gt-2dr-coupe.jpg",
+//     product_oder: "Oder1",
+// }
+// for (let i = 0; i < arr.length; i++) {
+//     const cardHTML =
+//         `<div class="card" style="width: 22rem;">
+//             <img src="${arr[i].Image_url}" class="card-img-top" alt="....">
+//             <div class="card-body">
+//                 <h5 class="card-title">${arr[i].title}</h5>
+//                 <p class="card-text">${arr[i].description}</p>
+//                 ${arr[i].prise}
+//                 <a href="#" class="btn btn-primary">${arr[i].product_oder}</a>
+//             </div>
+//         </div>`
+// };
+// console.log(arr);
+
+// ======================EXTRA WORK END=========================
